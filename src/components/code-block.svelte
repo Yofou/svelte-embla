d17056da00d0c16a63fabcd7aee57ae8782feab5
Yofou <exodias1965@gmail.com>
2022-04-23 16:40:41 +0100

    [📦] reduced highlight.js bundle

--- src/components/code-block.svelte
+++ src/components/code-block.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import HighlightSvelte from 'highlightjs-svelte';
+	import bash from "highlight.js/lib/languages/bash"
+	import xml from "highlight.js/lib/languages/xml"
+	import javascript from "highlight.js/lib/languages/javascript"
 
 	export let lang: string;
 	export let code: string;
 
 	let ref: HTMLElement;
 	onMount(async () => {
-		const { default: Highlight } = await import('highlight.js');
+		const { default: Highlight } = await import('highlight.js/lib/core');
+		Highlight.registerLanguage("bash", bash)
+		Highlight.registerLanguage("xml", xml)
+		Highlight.registerLanguage("javascript", javascript)
 		HighlightSvelte(Highlight);
 
 		Highlight.highlightElement(ref);
