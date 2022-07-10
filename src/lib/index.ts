ccc3d9d7b1fde167f2bcf98fb64f908b4cf94866
Yofou <exodias1965@gmail.com>
2022-07-10 12:20:58 +0100

    [📦] Added Re-init proposal on config changes

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,21 +2,20 @@ import EmblaCarousel from 'embla-carousel';
 import type { Action } from 'svelte/action';
 import type { EmblaOptionsType, EmblaPluginType, EmblaCarouselType } from 'embla-carousel';
 import type { Writable } from 'svelte/store';
-import { paramCase } from "param-case"
+import { paramCase } from 'param-case';
+import { arePluginsEqual } from './utils';
 
 export const emblaEventType = [
-	'init',
-	'pointerDown',
-	'pointerUp',
-	'scroll',
-	'select',
-	'settle',
-	'destroy',
-	'reInit',
-	'resize'
+	"init",
+	"pointerDown",
+	"pointerUp",
+	"scroll",
+	"select",
+	"settle",
+	"destroy",
+	"reInit",
+	"resize",
 ] as const;
-
-export type EmblaEventType = typeof emblaEventType[number];
 
 export type EmbaOptionsActionType = EmblaOptionsType &
 	Partial<{
@@ -24,15 +23,17 @@ export type EmbaOptionsActionType = EmblaOptionsType &
 		store: Writable<EmblaCarouselType>;
 	}>;
 
+const areOptionsEqual = EmblaCarousel.optionsHandler().areEqual;
 const action: Action<HTMLElement, EmbaOptionsActionType> = (node, options = {}) => {
+	let cachedOptions = options;
 	const plugins = options?.plugins ?? [];
-	const embla = EmblaCarousel(node, options, plugins);
+	const embla = EmblaCarousel(node, cachedOptions, plugins);
 	options?.store?.set(embla);
 
 	emblaEventType.forEach((eventType) => {
-		const options: CustomEventInit<{ embla?: EmblaCarouselType }> = { detail: {} }
-		const eventName = `e-${paramCase(eventType)}`
-		if (eventType === "init") options.detail.embla = embla
+		const options: CustomEventInit<{ embla?: EmblaCarouselType }> = { detail: {} };
+		const eventName = `e-${paramCase(eventType)}`;
+		if (eventType === 'init') options.detail.embla = embla;
 		embla.on(eventType, () => node.dispatchEvent(new CustomEvent(eventName, options)));
 	});
 
@@ -40,6 +41,18 @@ const action: Action<HTMLElement, EmbaOptionsActionType> = (node, options = {}) 
 		destroy: () => {
 			embla.destroy();
 			options?.store?.set(undefined);
+		},
+		update: (newEmblaConfig) => {
+			const optionsChanged = !areOptionsEqual(cachedOptions, newEmblaConfig);
+			const pluginsChanged = !arePluginsEqual(cachedOptions.plugins, newEmblaConfig.plugins);
+
+			// If new options or plugins are equal to current, bail here.
+			if (!optionsChanged && !pluginsChanged) return;
+
+			// If options or plugins have changed, store them and reInit embla
+			cachedOptions = newEmblaConfig;
+			embla.reInit(cachedOptions, cachedOptions.plugins);
+			options?.store?.set(embla)
 		}
 	};
 };
---
+++ src/lib/utils.ts
@@ -0,0 +1,22 @@
+import EmblaCarousel from 'embla-carousel';
+import type { EmblaPluginType } from 'embla-carousel';
+
+export function sortAndMapPluginToOptions(plugins: EmblaPluginType[]) {
+	return plugins
+		.concat()
+		.sort((a, b) => (a.name > b.name ? 1 : -1))
+		.map((plugin) => plugin.options);
+}
+
+export function arePluginsEqual(pluginsA: EmblaPluginType[], pluginsB: EmblaPluginType[]) {
+	if (pluginsA.length !== pluginsB.length) return false;
+
+	const { areEqual } = EmblaCarousel?.optionsHandler();
+	const optionsA = sortAndMapPluginToOptions(pluginsA);
+	const optionsB = sortAndMapPluginToOptions(pluginsB);
+
+	return optionsA.every((optionA, index) => {
+		const optionB = optionsB[index];
+		return areEqual(optionA, optionB);
+	});
+}
